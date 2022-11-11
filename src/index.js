7cc4d61242bea8b60ac4c293e2ac3dd3532a41cd
Matt Iorio <85589086+msi43089@users.noreply.github.com>
2022-11-11 12:43:16 -0500

    Added Provider and Store to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
