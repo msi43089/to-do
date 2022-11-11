f80db220021c3bde2b7b64c8a7e4fe81585e2e23
Matt Iorio <85589086+msi43089@users.noreply.github.com>
2022-11-11 15:45:43 -0500

    Added reducer for the list

--- src/features/addListSlice.js
+++ src/features/addListSlice.js
@@ -2,17 +2,24 @@ import { createSlice, current } from "@reduxjs/toolkit";
 
 export const listNameSlice = createSlice({
   name: "listName",
-  initialState: "",
+  initialState: {
+    create: "",
+    list: [],
+  },
   reducers: {
     createListName: (state, action) => {
-      state = action.payload;
+      state.create = action.payload;
       console.log(current(state));
+    },
+    addListName: (state, action) => {
+      state.list.push(state.create)
+      state.create = ""
     },
   },
 });
 
 export const { createListName } = listNameSlice.actions;
 
-export const selectListName = (state) => state.listName;
+export const selectListName = (state) => state.listName.create;
 
 export default listNameSlice.reducer;
