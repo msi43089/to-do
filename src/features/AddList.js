556e79f933837d1d2774c12c07e3032d097560cb
Matt Iorio <85589086+msi43089@users.noreply.github.com>
2022-11-11 12:28:44 -0500

    Added Change handler

--- src/features/AddList.js
+++ src/features/AddList.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../app/App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectListItem, addListItem } from "./addListSlice";
+import { selectListName, addListName } from "./addListSlice";
 
 export default function AddList() {
+  const dispatch = useDispatch();
+  const listName = useSelector(selectListName);
+  
+  const handleListNameChange = (e) =>{
+    dispatch(addListName(e.target.value))
+  }
+             
+  
   return (
     <div className="addList">
       <h1 className="title">Enter your list name here</h1>
-      <input id="addList" type="text" placeholder="List Item" />
+      <input 
+        id="listName" 
+        type="text" 
+        placeholder="List Name"
+        onChange={handleListNameChange}
+        value={listName}
+        />
       <br />
       <button>Create List</button>
     </div>
